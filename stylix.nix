5a9c23752718cb5ff3e08bf6326983e7348805bd
hexfae <hexfae@proton.me>
2024-12-16 07:08:10 +0100

    use let bindings in stylix, try recursive font

--- stylix.nix
+++ stylix.nix
@@ -4,25 +4,27 @@
     sha256 = "sha256-S0MumuBGJulUekoGI2oZfUa/50Jw0ZzkqDDu1nRkFUA=";
   };
   theme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+  cursor.package = pkgs.posy-cursors;
+  cursor.name = "Posy_Cursor_Black";
+  sansSerif.package = pkgs.recursive;
+  sansSerif.name = "Recursive Sans Casual";
+  serif.package = pkgs.recursive;
+  serif.name = "Recursive Sans Linear";
+  monospace.package = pkgs.nerd-fonts.recursive-mono;
+  monospace.name = "Recursive Mono Casual";
+  emoji.package = pkgs.twitter-color-emoji;
+  emoji.name = "Twitter Color Emoji";
 in {
   stylix.enable = true;
   stylix.polarity = "dark";
   stylix.image = wallpaper;
   stylix.base16Scheme = theme;
-  stylix.cursor.package = pkgs.posy-cursors;
-  stylix.cursor.name = "Posy_Cursor_Black";
-  stylix.fonts.sansSerif.package = pkgs.roboto;
-  stylix.fonts.sansSerif.name = "Roboto";
-  stylix.fonts.serif.package = pkgs.roboto-slab;
-  stylix.fonts.serif.name = "Roboto Slab";
-  stylix.fonts.monospace.package = pkgs.maple-mono-NF;
-  stylix.fonts.monospace.name = "Maple Mono NF";
-  stylix.fonts.emoji.package = pkgs.twitter-color-emoji;
-  stylix.fonts.emoji.name = "Twitter Color Emoji";
+  stylix.cursor = cursor;
+  stylix.fonts.sansSerif = sansSerif;
+  stylix.fonts.serif = serif;
+  stylix.fonts.monospace = monospace;
+  stylix.fonts.emoji = emoji;
   stylix.fonts.sizes.terminal = 16;
-
-  # stylix.targets.grub.useImage = true;
-  # stylix.targets.plymouth.enable = false;
 
   home-manager.users.hexfae.gtk.enable = true;
   home-manager.users.hexfae.stylix.polarity = "dark";
@@ -30,16 +32,11 @@ in {
   home-manager.users.hexfae.gtk.iconTheme.name = "Papirus-Dark";
   home-manager.users.hexfae.stylix.enable = true;
   home-manager.users.hexfae.stylix.image = wallpaper;
-  home-manager.users.hexfae.stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
-  home-manager.users.hexfae.stylix.cursor.package = pkgs.posy-cursors;
-  home-manager.users.hexfae.stylix.cursor.name = "Posy_Cursor_Black";
-  home-manager.users.hexfae.stylix.fonts.serif.package = pkgs.roboto-slab;
-  home-manager.users.hexfae.stylix.fonts.serif.name = "Roboto Slab";
-  home-manager.users.hexfae.stylix.fonts.sansSerif.package = pkgs.roboto;
-  home-manager.users.hexfae.stylix.fonts.sansSerif. name = "Roboto";
-  home-manager.users.hexfae.stylix.fonts.monospace.package = pkgs.maple-mono-NF;
-  home-manager.users.hexfae.stylix.fonts.monospace.name = "Maple Mono NF";
-  home-manager.users.hexfae.stylix.fonts.emoji.package = pkgs.twitter-color-emoji;
-  home-manager.users.hexfae.stylix.fonts.emoji.name = "Twitter Color Emoji";
+  home-manager.users.hexfae.stylix.base16Scheme = theme;
+  home-manager.users.hexfae.stylix.cursor = cursor;
+  home-manager.users.hexfae.stylix.fonts.sansSerif = sansSerif;
+  home-manager.users.hexfae.stylix.fonts.serif = serif;
+  home-manager.users.hexfae.stylix.fonts.monospace = monospace;
+  home-manager.users.hexfae.stylix.fonts.emoji = emoji;
   home-manager.users.hexfae.stylix.fonts.sizes.terminal = 16;
 }

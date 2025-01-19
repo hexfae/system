fa2614ef257fbca338aeead1aeeae9387c5bb57a
hexfae <hexfae@proton.me>
2025-01-19 11:50:48 +0100

    finish laptop configuration

---
+++ laptop.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  imports = [./hardware/laptop-hardware-configuration.nix ./boot.nix ./gnome.nix ./home.nix ./i18n.nix ./nix.nix ./programs ./stylix.nix ./system.nix ./user.nix];
+
+  networking.hostName = "laptop";
+  # jovian.steam.enable = true;
+  programs.steam.enable = true;
+  programs.steam.extraCompatPackages = [pkgs.proton-ge-bin];
+  programs.steam.extest.enable = true;
+  home-manager.users.hexfae.home.packages = with pkgs; [
+    obs-studio
+    dolphin-emu
+    lime3ds
+    prismlauncher
+    lutris
+    blender
+    librewolf
+    bottles
+    adwsteamgtk
+  ];
+
+  system.stateVersion = "24.05";
+}

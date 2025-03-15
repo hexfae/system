6b72d5b453aa36d85e66283ff6b715930308221d
hexfae <hexfae@proton.me>
2025-03-15 16:05:44 +0100

    add openrgb

--- machines/desktop.nix
+++ machines/desktop.nix
@@ -16,6 +16,7 @@
 
     ''ATTR{power/wakeup}="disabled"''
   ];
+  services.hardware.openrgb.enable = true;
   programs.steam.enable = true;
   programs.steam.extraCompatPackages = [pkgs.proton-ge-bin];
   programs.steam.extest.enable = true;

f163e1ac5ed3f9a35d4b481af124d0c996caa78c
hexfae <hexfae@proton.me>
2025-03-25 00:34:55 +0100

    add github credentials to agenix

--- programs/git.nix
+++ programs/git.nix
@@ -1,5 +1,11 @@
-{...}: {
+{config, ...}: {
   home-manager.users.hexfae.programs.git.enable = true;
   home-manager.users.hexfae.programs.git.userName = "hexfae";
   home-manager.users.hexfae.programs.git.userEmail = "hexfae@proton.me";
+  age.secrets.github.file = ../secrets/github.age;
+  age.secrets.github.owner = "hexfae";
+  # change path because git needs the folder where the credentials are stored to be writable to create a lock file
+  age.secrets.github.path = "/home/hexfae/.config/git/credentials";
+  age.secrets.github.name = "credentials";
+  home-manager.users.hexfae.programs.git.extraConfig.credential.helper = "store --file ${config.age.secrets.github.path}";
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,6 +6,9 @@ let
   desktop_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUSt7OfirUqrJVp1Gv0opWaD7klidgmvRddDXw4qYzj root@desktop";
   server_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3rgqbhUiuc5MV+niwye5UluNH/ERtBoUbrtZIixrPp root@server";
   systems = [desktop_host server_host];
+
+  all = users ++ systems;
 in {
-  "transmission.age".publicKeys = users ++ systems;
+  "transmission.age".publicKeys = all;
+  "github.age".publicKeys = all;
 }

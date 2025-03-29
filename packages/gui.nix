490eddd9a80576e469104dda2d9bac4511cf43c4
hexfae <hexfae@proton.me>
2025-03-29 19:59:46 +0100

    remove windsurf files, update windsurf overlay to 1.5.9

--- packages/gui.nix
+++ packages/gui.nix
@@ -2,10 +2,10 @@
   nixpkgs.overlays = [
     (final: prev: {
       windsurf = prev.windsurf.overrideAttrs (oldAttrs: {
-        version = "1.5.6";
+        version = "1.5.9";
         src = prev.fetchurl {
-          url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/164066c0badcfdea724847b1a24fd88eb96f9510/Windsurf-linux-x64-1.5.6.tar.gz";
-          sha256 = "5b01ce09139d7d8932be5c297a1c71a891a299825b2d5304f3fed22367188ecb";
+          url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/b3241b91445f79878ccc91626dfe190f90563e53/Windsurf-linux-x64-1.5.9.tar.gz";
+          sha256 = "sha256-3jPS64q1qiTuRbzA+K9vlzMR8bSaLA4xw/EODGN0z3M=";
         };
       });
     })

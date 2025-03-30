dc34204606f7001741d4b42224dff1860981025b
hexfae <hexfae@proton.me>
2025-03-30 20:32:08 +0200

    open some firewall ports for steam

--- programs/steam.nix
+++ programs/steam.nix
@@ -2,4 +2,7 @@
   programs.steam.enable = true;
   programs.steam.extraCompatPackages = [pkgs.proton-ge-bin];
   programs.steam.extest.enable = true;
+  programs.steam.remotePlay.openFirewall = true;
+  programs.steam.dedicatedServer.openFirewall = true;
+  programs.steam.localNetworkGameTransfers.openFirewall = true;
 }

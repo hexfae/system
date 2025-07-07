989d4e0a0d7d85363fb40deb351ca74b97d99ce6
hexfae <hexfae@proton.me>
2025-07-07 22:54:08 +0200

    remove agenix github token secret that broke new installations

--- programs/git.nix
+++ programs/git.nix
@@ -3,14 +3,6 @@
     enable = true;
     userName = "hexfae";
     userEmail = "hexfae@proton.me";
+    extraConfig.credential.helper = "store";
   };
-  age.secrets.github = {
-    file = ../secrets/github.age;
-    owner = "hexfae";
-    # change path because git needs the folder where the credentials are stored to be writable to create a lock file (TODO: FIXME: i'm not sure this actually fixes it)
-    path = "/home/hexfae/.config/git/credentials";
-    name = "credentials";
-  };
-  # home-manager.users.hexfae.programs.git.extraConfig.credential.helper = "store --file ${config.age.secrets.github.path}";
-  home-manager.users.hexfae.programs.git.extraConfig.credential.helper = "store";
 }

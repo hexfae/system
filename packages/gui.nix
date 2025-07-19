2787e5b85d3d5388ab306e22dc93216f07d66a8c
hexfae <hexfae@proton.me>
2025-07-19 02:26:41 +0200

    add portal configuration to packages/gui.nix

--- packages/gui.nix
+++ packages/gui.nix
@@ -1,26 +1,47 @@
 {pkgs, ...}: {
-  home-manager.users.hexfae.home = {
-    sessionVariables = {
-      GDK_BACKEND = "wayland,x11,*";
-      QT_QPA_PLATFORM = "wayland;xcb";
-      SDL_VIDEODRIVER = "wayland";
-      CLUTTER_BACKEND = "wayland";
-      NIXOS_OZONE_WL = "1";
-      MOZ_USE_XINPUT2 = "1";
-      FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+  home-manager.users.hexfae = {
+    services.polkit-gnome.enable = true;
+    xdg = {
+      mimeApps.enable = true;
+      portal = {
+        enable = true;
+        xdgOpenUsePortal = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-gnome
+          gnome-keyring
+        ];
+        configPackages = with pkgs; [
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-gnome
+          gnome-keyring
+        ];
+      };
     };
-    packages = with pkgs; [
-      kdePackages.kdenlive
-      vesktop
-      gimp
-      obsidian
-      obs-studio
-      dolphin-emu
-      azahar
-      prismlauncher
-      blender-hip
-      librewolf
-      (pkgs.bottles.override {removeWarningPopup = true;})
-    ];
+    home = {
+      sessionVariables = {
+        GDK_BACKEND = "wayland,x11,*";
+        QT_QPA_PLATFORM = "wayland;xcb";
+        SDL_VIDEODRIVER = "wayland";
+        CLUTTER_BACKEND = "wayland";
+        ELECTRON_OZONE_PLATFORM_HINT = "auto";
+        NIXOS_OZONE_WL = "1";
+        MOZ_USE_XINPUT2 = "1";
+        FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+      };
+      packages = with pkgs; [
+        kdePackages.kdenlive
+        vesktop
+        gimp
+        obsidian
+        obs-studio
+        dolphin-emu
+        azahar
+        prismlauncher
+        blender-hip
+        librewolf
+        (pkgs.bottles.override {removeWarningPopup = true;})
+      ];
+    };
   };
 }

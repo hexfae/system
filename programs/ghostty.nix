982c66f997eda8674efa4301d2d133b7e4b561e6
hexfae <hexfae@proton.me>
2025-07-19 02:27:21 +0200

    set ghostty to default terminal in ghostty.nix (does this even work?)

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -1,6 +1,9 @@
 {...}: {
-  home-manager.users.hexfae.programs.ghostty = {
-    enable = true;
-    installBatSyntax = true;
+  home-manager.users.hexfae = {
+    programs.ghostty = {
+      enable = true;
+      installBatSyntax = true;
+    };
+    xdg.mimeApps.defaultApplications."x-terminal-emulator" = "com.mitchellh.ghostty.desktop";
   };
 }

cd11cae0699000855becb7472b72848bc6c75dce
hexfae <hexfae@proton.me>
2025-11-22 18:56:59 +0100

    make git and helix options (enabled by default)

--- programs/git.nix
+++ programs/git.nix
@@ -1,11 +1,22 @@
-{...}: {
-  home-manager.users.hexfae.programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = "hexfae";
-        email = "hexfae@proton.me";
-        init.defaultBranch = "main";
+{
+  lib,
+  config,
+  ...
+}: {
+  options.huncs.programs.git.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+  };
+
+  config = lib.mkIf config.huncs.programs.git.enable {
+    home-manager.users.hexfae.programs.git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "hexfae";
+          email = "hexfae@proton.me";
+          init.defaultBranch = "main";
+        };
       };
     };
   };

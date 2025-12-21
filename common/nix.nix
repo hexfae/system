52f337e3c60ca36bad640a042378227b4488fe09
hexfae <hexfae@proton.me>
2025-12-21 20:42:06 +0100

    enable distributed builds and add server to remote build machines

--- common/nix.nix
+++ common/nix.nix
@@ -3,8 +3,21 @@
   documentation.nixos.enable = false;
   documentation.man.enable = false;
   documentation.enable = false;
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    use-xdg-base-directories = true;
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "129.151.192.240";
+        system = "aarch64-linux";
+        protocol = "ssh-ng";
+        supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+      }
+    ];
+    settings = {
+      experimental-features = "nix-command flakes";
+      use-xdg-base-directories = true;
+      auto-optimise-store = true;
+      builders-use-substitutes = true;
+    };
   };
 }

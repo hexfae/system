d1b4f3138a30142e66cdcfb41124ca4729eb650b
hexfae <hexfae@proton.me>
2025-12-23 23:36:53 +0100

    add some caches, auto optimise and garbage collect

--- common/nix.nix
+++ common/nix.nix
@@ -16,8 +16,24 @@
     settings = {
       experimental-features = "nix-command flakes";
       use-xdg-base-directories = true;
-      auto-optimise-store = true;
       builders-use-substitutes = true;
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://nyx.chaotic.cx"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      ];
+    };
+    optimise = {
+      automatic = true;
+      dates = "daily";
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than-7d";
     };
   };
 }

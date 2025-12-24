db7ad7e05c283f32d65f5d8148e2ff784e9350d0
hexfae <hexfae@proton.me>
2025-12-24 06:41:36 +0100

    set systemd-boot configuration limit to 10

--- common/boot.nix
+++ common/boot.nix
@@ -1,4 +1,9 @@
 {...}: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+  };
 }

cab30aeb1f2bc95b315c999bd1f57faf7534b406
hexfae <hexfae@proton.me>
2026-02-21 10:21:56 +0100

    reference stylix wallpaper in niri wallpaper config

--- modules/programs/niri/wallpaper.nix
+++ modules/programs/niri/wallpaper.nix
@@ -1,10 +1,11 @@
 {
   flake.modules = {
-    homeManager.niri = {pkgs, ...}: let
-      image = pkgs.fetchurl {
-        url = "https://w.wallhaven.cc/full/1k/wallhaven-1kpqw9.jpg";
-        sha256 = "sha256-C7w/kRuy5Ho+IeXkfECUYwsP+5pAkUdR9/zVEFsBrEU=";
-      };
+    homeManager.niri = {
+      pkgs,
+      osConfig,
+      ...
+    }: let
+      image = osConfig.stylix.image;
       blurred-image = pkgs.runCommand "blurred-wallpaper.png" {} ''
         ${pkgs.imagemagick}/bin/magick "${image}" -blur 0x8 $out
       '';

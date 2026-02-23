df9ddb6d711624065f41a57ef344e17183e24eb7
hexfae <hexfae@proton.me>
2026-02-23 10:11:27 +0100

    add some sysctl swap tweaks

--- modules/system/zram/zram.nix
+++ modules/system/zram/zram.nix
@@ -1,1 +1,11 @@
-{flake.modules.nixos.zram.zramSwap.enable = true;}
+{
+  flake.modules.nixos.zram = {
+    zramSwap.enable = true;
+    boot.kernel.sysctl = {
+      "vm.swappiness" = 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+      "vm.page-cluster" = 0;
+    };
+  };
+}

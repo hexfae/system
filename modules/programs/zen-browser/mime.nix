0b267d76b628b57bfd28b0651ad787c3b5f4f97b
hexfae <hexfae@proton.me>
2026-03-07 01:10:23 +0100

    add zen browser mime configuration

---
+++ modules/programs/zen-browser/mime.nix
@@ -0,0 +1,33 @@
+{inputs, ...}: {
+  flake.modules.homeManager.zen-browser = {pkgs, ...}: {
+    xdg.mimeApps = let
+      value = let
+        zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.twilight;
+      in
+        zen-browser.meta.desktopFileName;
+
+      associations = builtins.listToAttrs (map (name: {
+          inherit name value;
+        }) [
+          "application/x-extension-shtml"
+          "application/x-extension-xhtml"
+          "application/x-extension-html"
+          "application/x-extension-xht"
+          "application/x-extension-htm"
+          "x-scheme-handler/unknown"
+          "x-scheme-handler/mailto"
+          "x-scheme-handler/chrome"
+          "x-scheme-handler/about"
+          "x-scheme-handler/https"
+          "x-scheme-handler/http"
+          "application/xhtml+xml"
+          "application/json"
+          "text/plain"
+          "text/html"
+        ]);
+    in {
+      associations.added = associations;
+      defaultApplications = associations;
+    };
+  };
+}

60db53ada242c435a1c871c8e37d7ae4010a21f7
hexfae <hexfae@proton.me>
2026-04-19 00:15:00 +0200

    add sshuser and sshkey to nix remote builds aspect

--- modules/system/nix/remote-builds.nix
+++ modules/system/nix/remote-builds.nix
@@ -8,6 +8,8 @@
           hostName = config.constants.networking.server.ip;
           system = "aarch64-linux";
           protocol = "ssh-ng";
+          sshUser = config.constants.username;
+          sshKey = "${config.constants.home}/.ssh/id_ed25519";
           supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
         }
       ];
